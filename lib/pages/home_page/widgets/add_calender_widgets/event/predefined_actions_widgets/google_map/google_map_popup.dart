508d1dbfc9949a0bb931bed2955d666928e216f0
Tedsterh <56851744+Tedsterh@users.noreply.github.com>
2020-06-29 01:38:55 +0100

    completed_google_map_places_search

---
+++ lib/pages/home_page/widgets/add_calender_widgets/event/predefined_actions_widgets/google_map/google_map_popup.dart
@@ -0,0 +1,153 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:search_map_place/search_map_place.dart';
+
+class GoogleMapPopup extends StatefulWidget {
+  GoogleMapPopup({Key key}) : super(key: key);
+
+  @override
+  _GoogleMapPopupState createState() => _GoogleMapPopupState();
+}
+
+class _GoogleMapPopupState extends State<GoogleMapPopup> {
+  Future<LocationData> getLocation() async {
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return null;
+      }
+    }
+
+    LocationData _locationData = await location.getLocation();
+    return _locationData;
+  }
+
+  Location location = new Location();
+
+  PermissionStatus _permissionGranted;
+
+  ValueNotifier<LatLng> locationData = ValueNotifier(null);
+
+  Completer<GoogleMapController> _controller = Completer();
+
+  ValueNotifier<Map<MarkerId, Marker>> marker = ValueNotifier({});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: AlertDialog(
+            backgroundColor: Colors.transparent,
+            contentPadding: EdgeInsets.all(0),
+            insetPadding: EdgeInsets.all(0),
+            content: Builder(builder: (context) {
+              var width = MediaQuery.of(context).size.width - 25;
+              var height = MediaQuery.of(context).size.height - 200;
+              return FutureBuilder<LocationData>(
+                  future: getLocation(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Container(
+                        width: width,
+                        height: height,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30)),
+                        child: Stack(
+                          children: <Widget>[
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: Colors.white,
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(30),
+                                child: ValueListenableBuilder<
+                                        Map<MarkerId, Marker>>(
+                                    valueListenable: marker,
+                                    builder: (context, markers, child) {
+                                      return GoogleMap(
+                                          zoomControlsEnabled: false,
+                                          onMapCreated:
+                                              (GoogleMapController controller) {
+                                            _controller.complete(controller);
+                                          },
+                                          markers:
+                                              Set<Marker>.of(markers.values),
+                                          initialCameraPosition: CameraPosition(
+                                              target: LatLng(
+                                                  snapshot.data.latitude,
+                                                  snapshot.data.longitude)));
+                                    }),
+                              ),
+                            ),
+                            Positioned(
+                                top: 20,
+                                left: 20,
+                                right: 20,
+                                child: SearchMapPlaceWidget(
+                                  apiKey:
+                                      'AIzaSyA3DOz6Nzqig2DNiIvOI6wDHAjxqTttGpk',
+                                  language: 'en',
+                                  radius: 30000,
+                                  location: LatLng(snapshot.data.latitude,
+                                      snapshot.data.longitude),
+                                  onSelected: (value) async {
+                                    Geolocation geo = await value.geolocation;
+                                    locationData.value = LatLng(
+                                        geo.coordinates.latitude,
+                                        geo.coordinates.longitude);
+                                    marker.value = <MarkerId, Marker>{
+                                      MarkerId('eventLocation'): Marker(
+                                          markerId: MarkerId('eventLocation'),
+                                          position: LatLng(
+                                              geo.coordinates.latitude,
+                                              geo.coordinates.longitude))
+                                    };
+                                    final GoogleMapController controller =
+                                        await _controller.future;
+                                    controller.animateCamera(
+                                        CameraUpdate.newCameraPosition(
+                                            CameraPosition(
+                                                target: LatLng(
+                                                    geo.coordinates.latitude,
+                                                    geo.coordinates.longitude),
+                                                zoom: 16)));
+                                  },
+                                )),
+                            Positioned(
+                                right: 20,
+                                bottom: 20,
+                                child: NeumorphicButton(
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .pop(locationData.value);
+                                  },
+                                  child: Text(
+                                    'Select',
+                                    style: TextStyle(fontSize: 22),
+                                  ),
+                                ))
+                          ],
+                        ),
+                      );
+                    } else {
+                      return Container(
+                        width: width,
+                        height: height,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30)),
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    }
+                  });
+            })));
+  }
+}
--- lib/pages/home_page/widgets/add_calender_widgets/event/predefined_actions_widgets/location.dart
+++ lib/pages/home_page/widgets/add_calender_widgets/event/predefined_actions_widgets/location.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:run_my_lockdown/pages/home_page/widgets/add_calender_widgets/event/predefined_actions_widgets/google_map/google_map_popup.dart';
 
 class LocationAction extends StatefulWidget {
   LocationAction(
@@ -15,65 +16,39 @@ class LocationAction extends StatefulWidget {
 }
 
 class _LocationActionState extends State<LocationAction> {
-  Future<LocationData> getLocation() async {
-    _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return null;
-      }
-    }
-
-    LocationData _locationData = await location.getLocation();
-    return _locationData;
-  }
-
-  Location location = new Location();
-
-  PermissionStatus _permissionGranted;
-
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 2.0, bottom: 10.0),
-      child: FutureBuilder<LocationData>(
-          future: getLocation(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListTile(
-                leading: ValueListenableBuilder<List<String>>(
-                    valueListenable: widget.actionList,
-                    builder: (context, list, child) {
-                      return NeumorphicCheckbox(
-                          value: list.contains('location'),
-                          onChanged: (value) async {
-                            if (value) {
-                              List newList =
-                                  List<String>.from(widget.actionList.value);
-                              newList.add('location');
-                              widget.eventLocation.value = LatLng(
-                                  snapshot.data.latitude,
-                                  snapshot.data.longitude);
-                              widget.actionList.value = newList;
-                            } else {
-                              List newList =
-                                  List<String>.from(widget.actionList.value);
-                              newList.remove('location');
-                              widget.eventLocation.value = null;
-                              widget.actionList.value = newList;
-                            }
-                          });
-                    }),
-                title: Text('Add location point'),
-              );
-            } else {
-              return ListTile(
-                title: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              );
-            }
-          }),
-    );
+        padding: const EdgeInsets.only(top: 2.0, bottom: 10.0),
+        child: ListTile(
+          leading: ValueListenableBuilder<List<String>>(
+              valueListenable: widget.actionList,
+              builder: (context, list, child) {
+                return NeumorphicCheckbox(
+                    value: list.contains('location'),
+                    onChanged: (value) async {
+                      if (value) {
+                        List newList =
+                            List<String>.from(widget.actionList.value);
+                        newList.add('location');
+                        LatLng _location = await showDialog(
+                            context: context,
+                            barrierDismissible: true,
+                            builder: (BuildContext context) {
+                              return GoogleMapPopup();
+                            });
+                        widget.eventLocation.value = _location;
+                        widget.actionList.value = newList;
+                      } else {
+                        List newList =
+                            List<String>.from(widget.actionList.value);
+                        newList.remove('location');
+                        widget.eventLocation.value = null;
+                        widget.actionList.value = newList;
+                      }
+                    });
+              }),
+          title: Text('Add location point'),
+        ));
   }
 }
